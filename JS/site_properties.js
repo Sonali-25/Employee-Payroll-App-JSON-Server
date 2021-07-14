21fce3d2dc519032cb9a1b6bb2229485d3df1da0
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-14 20:09:08 +0530

    Employee Payroll App-JSON Server

---
+++ JS/site_properties.js
@@ -0,0 +1,6 @@
+let site_properties = {
+    home_page : "../HTML/EmployeePayroll.html",
+    add_employee_page : "../HTML/AddEmployeePayroll.html",
+    use_local_storage:"true",
+    server_url:"http://localhost:3000/EmployeePayrollDB/"
+}
